415a6371f340d6b0b379459a837e4d765f196e5b
Phong Nguyen <secretrare712@gmail.com>
2025-02-20 15:27:01 +0700

    part 5

---
+++ Group_14/ServiceLayer/Service/MovieService.cs
@@ -0,0 +1,29 @@
+﻿using Group_14.MovieReview.CoreLayer.Entities;
+
+namespace Group_14.ServiceLayer.Service
+{
+    public class MovieService:IMovieService
+    {
+        private readonly IMovieRepository _movieRepository;
+        public MovieService(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+        // CRUD Methods
+        public async Task<IEnumerable<Movie>> GetAllMoviesAsync()
+        {
+            return await _movieRepository.GetAllMoviesAsync();
+        }
+        public async Task AddMovieAsync(Movie movie)
+        {
+            await _movieRepository.AddMovieAsync(movie);
+        }
+        // Stored Procedure Method
+        public async Task<IEnumerable<Movie>>
+       GetTopRatedMoviesWithSpAsync(int topCount)
+        {
+            return await
+           _movieRepository.GetTopRatedMoviesWithSpAsync(topCount);
+        }
+    }
+}
